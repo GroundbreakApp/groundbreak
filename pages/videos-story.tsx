640e5e5a6e09c4ce50358a5f24f16e8046fc6597
zfr77 <zfr@zbalbous.com>
2023-05-22 17:34:11 +0100

    devin's changes

--- pages/videos-story.tsx
+++ pages/videos-story.tsx
@@ -179,7 +179,7 @@ const stories2 = [
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <div className="stories">
         <Suspense>
           <StoriesLazy
