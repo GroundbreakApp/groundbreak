2ed250631e1161868514b15deb1e4eb533099907
zfr77 <zfr@zbalbous.com>
2023-05-24 14:27:22 +0100

    overlay

--- pages/videos-story.tsx
+++ pages/videos-story.tsx
@@ -189,12 +189,12 @@ const stories2 = [
 
 function App() {
   return (
-    <div className="App" style={{ display: 'grid', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div className="App" style={{ display: 'grid', justifyContent: 'center', alignItems: 'center', height: 'max(100svh, 95%' }}>
       <div className="stories">
         <Suspense>
           <StoriesLazy
-            height="100vh"
-            width="calc(100svh * 9 / 16)"
+            height="max(100svh, 95%)"
+            width="calc(max(100svh, 95%) * 9 / 16)"
             loop
             keyboardNavigation
             defaultInterval={8000}
