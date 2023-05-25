2315c820164b42b7a1489c1a31dc8d9723f3b816
zfr77 <zfr@zbalbous.com>
2023-05-25 19:03:54 +0100

    css

--- pages/videos-story.tsx
+++ pages/videos-story.tsx
@@ -1,221 +1,228 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-import React, { Suspense, useEffect, useState } from "react";
-import StoriesLazy from '@/components/story'
-import LinkSVG from "@/assets/link.svg";
-const stories2 = [
-  {
-    playbackId: "4vipwgRq4p01pYskY5oGjM02fwL6kGA00eG53ux02aQRl5w",
-    duration: 9.7 * 1000,
-    type: "mux-video",
-    isAutoplay: false,
-    isFirstSlide: true,
-  },
-  {
-    playbackId: "SmaCpxZUcHzrwE6qIuIzyrjwWSPy00OFWExu4waY2WA8", // groundbreak is short form video
-    type: "mux-video",
-    duration: 5 * 1000,
-    isAutoplay: true,
-  },
-  {
-    playbackId: "6Rask9izbjGHMBGZiFS5xMoEDV2TYB6of27cuqpvIZk", // groundbreak is short form video
-    type: "mux-video",
-    duration: 22.055367 * 1000,
-    isAutoplay: true,
-    widgets: [{
-      spawnTime: 13000,
-      duration: 8500,
-      render: () => {
-        return (
-          <div style={{
-            position: "absolute",
-            left: 0,
-            right: 0,
-            width: "100%",
-            height: "100%",
-            zIndex: "9999",
-            pointerEvents: "none"
-          }}>
-            <a
-              href="https://docs.google.com/presentation/d/1oNcfzR-yjERDBtwihW86UqKGfvi0AR8T/edit?usp=sharing&ouid=113854719469946467137&rtpof=true&sd=true"
-              target="_blank"
-              style={{
-                pointerEvents: "auto",
-                position: "absolute",
-                left: "27%",
-                top: "66%",
-                padding: "10px 20px",
-                fontSize: "24px",
-                color: "black",
-                display: "flex",
-                alignItems: "center",
-                cursor: "pointer",
-                borderRadius: "40px",
-                border: "5px solid black"
-              }}
-            >
-              <span style={{
-                marginRight: "10px"
-              }}>
-                <LinkSVG />
-              </span>
-              Kickoff Deck </a>
-          </div>
-        )
-      }
-    }]
-  },
-  {
-    playbackId: "DhjdILWdsQSoqUs2ps9nFLs02K00c4Dx2hP1Iyemd8oAw", // best sales tool
-    duration: 25.133333 * 1000,
-    type: "mux-video",
-    isAutoplay: true,
-  },
-  {
-    playbackId: "AAjzwXeiO551UAvjID8fPS02bESiRX8jwyqUj024gcA28",
-    duration: 15.081733 * 1000,
-    type: "mux-video",
-    isAutoplay: true,
-  },
-  {
-    playbackId: "I01aOPryQi5hVuir02mqM02B3G01g45s00EFDaGzqlS3bQY4",
-    duration: 11 * 1000,
-    type: "mux-video",
-    isAutoplay: true,
-  },
-  {
-    playbackId: "00CzDwETt01Sh1eNqIgp4DM01008SqWdqbeGEIkkAITagp8",
-    duration: 10 * 1000,
-    type: "mux-video",
-    isAutoplay: true,
-    widgets: [{
-      spawnTime: 0,
-      duration: 9000,
-      render: () => {
-        return (
-          <div style={{
-            position: "absolute",
-            left: 0,
-            right: 0,
-            width: "100%",
-            height: "100%",
-            zIndex: "9999",
-            pointerEvents: "none"
-          }}>
-            <a
-              href=" https://docs.google.com/document/d/1FlVj-62uJBrHadc_A3il2e6RWnGmJ-XBbU1F-Q3RsvI/edit?usp=sharing"
-              target="_blank"
-              style={{
-                pointerEvents: "auto",
-                position: "absolute",
-                left: "20%",
-                top: "30%",
-                width: "60%",
-                padding: "10px",
-                fontSize: "24px",
-                color: "black",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                cursor: "pointer",
-                borderRadius: "40px",
-                border: "5px solid black"
-              }}
-            >
-              <span style={{
-                marginRight: "10px"
-              }}>
-                <LinkSVG />
-              </span>
-              Product Experiment </a>
-            <a
-              href="https://docs.google.com/spreadsheets/d/17vQW0nEmSmC_B6AhA_igT6vma1WYZlKI7MA6TsAVCYI/edit?usp=sharing"
-              target="_blank"
-              style={{
-                pointerEvents: "auto",
-                position: "absolute",
-                left: "20%",
-                top: "45%",
-                width: "60%",
-                padding: "10px",
-                fontSize: "24px",
-                color: "black",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                cursor: "pointer",
-                borderRadius: "40px",
-                border: "5px solid black"
-              }}
-            >
-              <span style={{
-                marginRight: "10px"
-              }}>
-                <LinkSVG />
-              </span>
-              Interview Questions</a>
-            <a
-              href="https://docs.google.com/spreadsheets/d/1Ku1U1IoHarK-h-eqt02YwvCMHfvLg3RQ41YbHb3YL9E/edit?usp=sharing"
-              target="_blank"
-              style={{
-                pointerEvents: "auto",
-                position: "absolute",
-                left: "20%",
-                top: "60%",
-                width: "60%",
-                padding: "10px",
-                fontSize: "24px",
-                color: "black",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                cursor: "pointer",
-                borderRadius: "40px",
-                border: "5px solid black"
-              }}
-            >
-              <span style={{
-                marginRight: "10px"
-              }}>
-                <LinkSVG />
-              </span>
-              Squad Assignments </a>
-          </div>
-        )
-      }
-    }]
-  },
-];
+// Styled components
+const StyledForm = styled.form`
+  position: relative;
+`;
 
+const StyledStoryPage = styled.div`
+  position: relative;
+  width: 414px;
+  height: 896px;
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0.2) 0%,
+    rgba(0, 0, 0, 0) 14.39%,
+    rgba(0, 0, 0, 0) 43.49%,
+    rgba(0, 0, 0, 0) 73.82%,
+    rgba(0, 0, 0, 0.2) 92.19%
+  );
+  background-image: url('new client.png');
+`;
 
-function App() {
-  const [height, setHeight] = useState('100vh'); // default to vh
+const StyledShowMoreIcon = styled.div`
+  position: absolute;
+  visibility: hidden;
+  width: 23px;
+  height: 24px;
+  left: 39.5px;
+  top: 55.5px;
+  transform: rotate(90deg);
+`;
 
-  useEffect(() => {
-    if ('CSS' in window && CSS.supports('height', '100svh')) {
-      setHeight('100svh'); // switch to svh if supported
-    }
-  }, []);
+const StyledProfilePicture = styled.div`
+  position: absolute;
+  visibility: hidden;
+  width: 47px;
+  height: 47px;
+  left: 360px;
+  top: 423px;
+`;
+
+const StyledLikeButton = styled.div`
+  position: absolute;
+  visibility: hidden;
+  width: 35px;
+  height: 56.25px;
+  left: 366px;
+  top: 501px;
+`;
+
+const StyledActionButton = styled.div`
+  position: absolute;
+  visibility: hidden;
+  width: 35px;
+  height: 56.25px;
+  left: 366px;
+  top: 579px;
+`;
+
+const StyledMusicDisc = styled.div`
+  position: absolute;
+  visibility: hidden;
+  width: 49px;
+  height: 49px;
+  left: 359px;
+  top: 732px;
+`;
+
+const StyledCommentBar = styled.div`
+  box-sizing: border-box;
+  position: absolute;
+  visibility: hidden;
+  width: 414px;
+  height: 82.5px;
+  left: 0px;
+  top: 813px;
+  background: #121212;
+  border: 1px solid #333333;
+`;
+
+const StyledBottomInformations = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 11px;
+  gap: 7px;
+  position: absolute;
+  visibility: hidden;
+  width: 299px;
+  height: 92.65px;
+  left: 0px;
+  bottom: 104.35px;
+`;
+
+const StyledBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 8px 10px;
+  gap: 7px;
+  position: absolute;
+  width: 414px;
+  height: 21px;
+  left: 0px;
+  bottom: 83px;
+`;
+
+const StyledProgress = styled.div`
+  display: none;
+  width: 193.5px;
+  height: 2px;
+  background: rgba(255, 255, 255, 0.34);
+  border-radius: 10px;
+  flex: none;
+  order: 0;
+  flex-grow: 1;
+`;
+
+const StyledProgress2 = styled.div`
+  display: none;
+  width: 193.5px;
+  height: 2px;
+  background: rgba(255, 255, 255, 0.34);
+  border-radius: 10px;
+  flex: none;
+  order: 1;
+  flex-grow: 1;
+`;
+
+const StyledRectangle = styled.div`
+  box-sizing: border-box;
+  position: absolute;
+  width: 343px;
+  height: 43px;
+  background: rgba(203, 253, 85, 0.5);
+  border: 3px solid rgba(255, 255, 255, 0.5);
+  border-radius: 8px;
+`;
+
+const StyledSendFeedback = styled.div`
+  position: absolute;
+  width: 88px;
+  height: 13px;
+  font-family: 'Graphik';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 13px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const StyledSendFill = styled.div`
+  position: relative;
+  width: 25px;
+  height: 25px;
+  opacity: 0.5;
+`;
+
+const StyledSubtract = styled.div`
+  position: absolute;
+  width: 15.93px;
+  height: 15.93px;
+  left: 4.25px;
+  top: 4.83px;
+  background: #222222;
+`;
+
+const StyledVector121 = styled.div`
+  position: absolute;
+  left: 8.33%;
+  right: 16.67%;
+  top: 16.67%;
+  bottom: 8.33%;
+  background: #33363F;
+  border-radius: 1px;
+`;
+
+const StyledVector122 = styled.div`
+  position: absolute;
+  left: 33.33%;
+  right: 37.5%;
+  top: 37.5%;
+  bottom: 33.33%;
+  border: 2px solid #33363F;
+  border-radius: 2px;
+`;
+
+const App: React.FC = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Form submitted');
+    // Handle form submission logic
+  };
 
   return (
-    <div className="App" style={{ display: 'grid', justifyContent: 'center', alignItems: 'center', height }}>
-      <div className="stories">
-        <Suspense>
-          <StoriesLazy
-            height={height}
-            width="calc(100svh * 9 / 16)" // replace this with the corresponding svh calculation if it exists
-            loop
-            keyboardNavigation
-            defaultInterval={8000}
-            stories={stories2}
-            onStoryEnd={(s: any, st: any) => console.log("story ended", s, st)}
-            onAllStoriesEnd={(s: any, st: any) => console.log("all stories ended", s, st)}
-            onStoryStart={(s: any, st: any) => console.log("story started", s, st)}
-            storyContainerStyles={{ borderRadius: 8, overflow: "hidden" }}
-          />
-        </Suspense>
-      </div>
-    </div>
+    <StyledForm onSubmit={handleSubmit}>
+      <StyledStoryPage>
+        <StyledShowMoreIcon />
+        <StyledProfilePicture />
+        <StyledLikeButton />
+        <StyledActionButton />
+        <StyledActionButton />
+        <StyledMusicDisc />
+        <StyledCommentBar />
+        <StyledBottomInformations />
+        <StyledBar>
+          <StyledProgress />
+          <StyledProgress2 />
+        </StyledBar>
+      </StyledStoryPage>
+      <StyledRectangle />
+      <StyledSendFeedback>Send Feedback</StyledSendFeedback>
+      <StyledSendFill>
+        <StyledSubtract />
+        <StyledVector121 />
+        <StyledVector122 />
+      </StyledSendFill>
+      <button type="submit">Submit</button>
+    </StyledForm>
   );
-}
+};
 
 export default App;
--- pages/zoom.tsx
+++ pages/zoom.tsx
@@ -1,67 +1,216 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import styled from 'styled-components';
 
-const Form = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
+// Styled components
+const StyledStoryPage = styled.div`
+  position: relative;
+  width: 414px;
+  height: 896px;
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0.2) 0%,
+    rgba(0, 0, 0, 0) 14.39%,
+    rgba(0, 0, 0, 0) 43.49%,
+    rgba(0, 0, 0, 0) 73.82%,
+    rgba(0, 0, 0, 0.2) 92.19%
+  );
+`;
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    console.log("Submit clicked")
-    try {
-        console.log("Making get call to zoom")
+const StyledShowMoreIcon = styled.div`
+  position: absolute;
+  visibility: hidden;
+  width: 23px;
+  height: 24px;
+  left: 39.5px;
+  top: 55.5px;
+  transform: rotate(90deg);
+`;
 
-        const response = await axios.post('https://groundbreak.onrender.com/metrics/zoom', {
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-        },
-        );
+const StyledProfilePicture = styled.div`
+  position: absolute;
+  visibility: hidden;
+  width: 47px;
+  height: 47px;
+  left: 360px;
+  top: 423px;
+`;
 
-        console.log(response)
-        setFirstName('');
-        setLastName('');
-        setEmail('');
-    } catch (error) {
-        console.error('Error joining Zoom webinar:', error);
-    }
-  };
+const StyledLikeButton = styled.div`
+  position: absolute;
+  visibility: hidden;
+  width: 35px;
+  height: 56.25px;
+  left: 366px;
+  top: 501px;
+`;
 
+const StyledActionButton = styled.div`
+  position: absolute;
+  visibility: hidden;
+  width: 35px;
+  height: 56.25px;
+  left: 366px;
+  top: 579px;
+`;
+
+const StyledMusicDisc = styled.div`
+  position: absolute;
+  visibility: hidden;
+  width: 49px;
+  height: 49px;
+  left: 359px;
+  top: 732px;
+`;
+
+const StyledCommentBar = styled.div`
+  box-sizing: border-box;
+  position: absolute;
+  visibility: hidden;
+  width: 414px;
+  height: 82.5px;
+  left: 0px;
+  top: 813px;
+  background: #121212;
+  border: 1px solid #333333;
+`;
+
+const StyledBottomInformations = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 11px;
+  gap: 7px;
+  position: absolute;
+  visibility: hidden;
+  width: 299px;
+  height: 92.65px;
+  left: 0px;
+  bottom: 104.35px;
+`;
+
+const StyledBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 8px 10px;
+  gap: 7px;
+  position: absolute;
+  width: 414px;
+  height: 21px;
+  left: 0px;
+  bottom: 83px;
+`;
+
+const StyledProgress = styled.div`
+  display: none;
+  width: 193.5px;
+  height: 2px;
+  background: rgba(255, 255, 255, 0.34);
+  border-radius: 10px;
+  flex: none;
+  order: 0;
+  flex-grow: 1;
+`;
+
+const StyledProgress2 = styled.div`
+  display: none;
+  width: 193.5px;
+  height: 2px;
+  background: rgba(255, 255, 255, 0.34);
+  border-radius: 10px;
+  flex: none;
+  order: 1;
+  flex-grow: 1;
+`;
+
+const StyledRectangle = styled.div`
+  box-sizing: border-box;
+  position: absolute;
+  width: 343px;
+  height: 43px;
+  background: rgba(203, 253, 85, 0.5);
+  border: 3px solid rgba(255, 255, 255, 0.5);
+  border-radius: 8px;
+`;
+
+const StyledSendFeedback = styled.div`
+  position: absolute;
+  width: 88px;
+  height: 13px;
+  font-family: 'Graphik';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 13px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.5);
+`;
+
+const StyledSendFill = styled.div`
+  position: relative;
+  width: 25px;
+  height: 25px;
+  opacity: 0.5;
+`;
+
+const StyledSubtract = styled.div`
+  position: absolute;
+  width: 15.93px;
+  height: 15.93px;
+  left: 4.25px;
+  top: 4.83px;
+  background: #222222;
+`;
+
+const StyledVector121 = styled.div`
+  position: absolute;
+  left: 8.33%;
+  right: 16.67%;
+  top: 16.67%;
+  bottom: 8.33%;
+  background: #33363F;
+  border-radius: 1px;
+`;
+
+const StyledVector122 = styled.div`
+  position: absolute;
+  left: 33.33%;
+  right: 37.5%;
+  top: 37.5%;
+  bottom: 33.33%;
+  border: 2px solid #33363F;
+  border-radius: 2px;
+`;
+
+const App: React.FC = () => {
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="firstName">First Name:</label>
-        <input
-          type="text"
-          id="firstName"
-          value={firstName}
-          onChange={(event) => setFirstName(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="lastName">Last Name:</label>
-        <input
-          type="text"
-          id="lastName"
-          value={lastName}
-          onChange={(event) => setLastName(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <StyledStoryPage>
+        <StyledShowMoreIcon />
+        <StyledProfilePicture />
+        <StyledLikeButton />
+        <StyledActionButton />
+        <StyledActionButton />
+        <StyledMusicDisc />
+        <StyledCommentBar />
+        <StyledBottomInformations />
+        <StyledBar>
+          <StyledProgress />
+          <StyledProgress2 />
+        </StyledBar>
+      </StyledStoryPage>
+      <StyledRectangle />
+      <StyledSendFeedback>Send Feedback</StyledSendFeedback>
+      <StyledSendFill>
+        <StyledSubtract />
+        <StyledVector121 />
+        <StyledVector122 />
+      </StyledSendFill>
+    </div>
   );
 };
 
-export default Form;
-
-
+export default App;
