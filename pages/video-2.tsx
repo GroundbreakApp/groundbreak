1ad73613792201fa5c7055e3035356d578c46a15
baymax <pony.kadeem@gmail.com>
2023-05-30 20:33:14 -0400

    feat: video 1 update, video 2 published

--- pages/video-2.tsx
+++ pages/video-2.tsx
@@ -1,101 +1,20 @@
 
 import React, { Suspense, useState, useEffect } from "react";
 import MyStory from '@/components/my-story'
-import LinkSVG from "@/assets/link.svg";
+import UpLeft from "@/assets/up-left.svg";
 import styled from 'styled-components';
 import axios from 'axios';
 import { TbSend } from "react-icons/tb"
 
 import { isMobile } from 'react-device-detect';
 
-import linkImage from '../assets/Subtract.png';
-import UpLeft from '../assets/up-left.svg';
 import { useAppDispatch } from "@/stores/hook";
 import { setPause } from "@/components/my-story/slices/story.slice";
 
-const StyledForm = styled.form`
-  position: absolute;
-  bottom: 8px;
-  left: 0;
-  right: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgba(0, 0, 0, 0.5); // semi-transparent background
-  padding: 10px;
-`;
-
-const StyledInput = styled.input`
-  box-sizing: border-box;
-  flex-grow: 1;
-  background: transparent;
-  border: none;
-  font-family: 'Graphik';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 25px;
-  color: white;
-`;
-
-const StyledIcon = styled.div`
-  width: 25px;
-  height: 25px;
-  opacity: 0.5;
-  background: #33363F;
-  border-radius: 1px;
-`;
-
-const FeedbackForm = () => {
-  const [feedbackText, setFeedbackText] = useState(''); // Add state for feedback text
-  const handleIconClick = async () => {
-
-    console.log("Submit clicked")
-    try {
-      console.log("Making call to send feedback")
-
-      const response = await axios.post('https://groundbreak.onrender.com/metrics/feedback', {
-        text: feedbackText,
-      });
-
-      console.log(response)
-      setFeedbackText('');
-    } catch (error) {
-      console.error('Error sending feedback call:', error);
-    }
-  };
-  const dispatch = useAppDispatch();
-  return (
-    <div style={{
-      position: "absolute",
-      left: 0,
-      right: 0,
-      width: "100%",
-      height: "100%",
-      zIndex: "20000",
-    }}>
-      <StyledForm>
-        <StyledInput
-          placeholder="Send Feedback"
-          value={feedbackText} // Set the value of StyledInput to feedbackText state
-          onChange={(event: any) => setFeedbackText(event.target.value)} // Update feedbackText state on input change
-          onFocus={() => dispatch(setPause(true))}
-        />
-        {/* <StyledIcon onClick={handleIconClick as () => void} style={{ backgroundImage: `url(${linkImage})` }} /> */}
-        <button className="h-8 w-8 text-white fill-current"
-          onClick={handleIconClick}
-        >
-
-          <TbSend />
-        </button>
-      </StyledForm>
-    </div>
-  )
-}
 
 const stories2 = [
   {
-    playbackId: "00qf3wmH00DHhiTZl8oe8RyulJPpa01jfQClmC5PbnAZ7Y",
+    playbackId: "lEkxLDgI9UtYHavO02tMkeFiOb7JhalcI013mQYavsTpE",
     duration: 12.8 * 1000,
     type: "mux-video",
     isAutoplay: false,
@@ -103,14 +22,49 @@ const stories2 = [
   },
 
   {
-    playbackId: "3P00vfla6A2ds7wBnZIsZEDenPent9RwobTDi3GYWEfI", // groundbreak is short form video
+    playbackId: "txu6bFcw502HhhA2SkZc14pUujOwQi1lZzrNEIkULIkQ", // groundbreak is short form video
     type: "mux-video",
-    duration: 15.6 * 1000,
+    duration: 4.1 * 1000,
     isAutoplay: true,
     overlayColor: "#EFE4AF",
   },
   {
-    playbackId: "Lrt72Y00TT2XyT6tqZ8OxbLDatFNt02eBLvbuAAju7LD00", // groundbreak is short form video
+    playbackId: "pV02Txnt7SFyO6OAhH3W5o6IWwd5Zpfc02r7meaKimPt00", // groundbreak is short form video
+    type: "mux-video",
+    duration: 3.6 * 1000,
+    isAutoplay: true,
+    overlayColor: "#659DAD",
+  },
+  {
+    playbackId: "3I9gOOM2LpVqjOv00ejZx3nYQ2q5xluD4HtSez02MDN00w", // groundbreak is short form video
+    type: "mux-video",
+    duration: 4.0 * 1000,
+    isAutoplay: true,
+    overlayColor: "#659DAD",
+  },
+  {
+    playbackId: "b5NrVCRkAobl02NKKtf96HkXzIKzdnBuRlKopG8ZZRpI", // groundbreak is short form video
+    type: "mux-video",
+    duration: 3.5 * 1000,
+    isAutoplay: true,
+    overlayColor: "#659DAD",
+    widgets: [
+      {
+        spawnTime: 0,
+        duration: 7000,
+        render: () => {
+          return <div className="flex justify-center items-start h-full w-full absolute left-0 top-0 z-[99999] ">
+            <a href="http://pmf.highalpha.com" target="_blank" className="mt-4 bg-[#FFBB86] text-[#172358] rounded-lg font-semibold py-2 flex items-center justify-center px-2">
+              View the Report
+              <span className="ml-2 w-4 h-4"> <UpLeft /> </span>
+            </a>
+          </div>
+        }
+      },
+    ]
+  },
+  {
+    playbackId: "4W00hZ4pMCakw02ndar56omDgA5sWguaflfc002ckR7qwU", // groundbreak is short form video
     type: "mux-video",
     duration: 7.7 * 1000,
     isAutoplay: true,
