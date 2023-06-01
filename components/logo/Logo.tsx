5c8b9bc6b6a2b35bf80aa53bfb514b5802f6b78d
baymax <pony.kadeem@gmail.com>
2023-06-01 14:58:21 -0400

    feat: logo is redirected to landing page

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -1,9 +1,13 @@
 import LogoSVG from "@/assets/groundbreak.svg";
 import logoUrl from "@/assets/groundbreak.png";
 import Image from "next/image";
+import { useRouter } from "next/router";
 export const Logo = () => {
+  const router = useRouter();
   return (<>
-    <div className="w-[189px] h-[23px] flex items-center absolute left-0 right-0 bottom-4 p-1 justify-center ml-auto mr-auto sm:ml-0 z-[9999] pointer-events-none">
+    <div className="w-[189px] h-[23px] flex items-center absolute left-0 right-0 bottom-4 p-1 justify-center ml-auto mr-auto sm:ml-0 z-[9999] pointer-events-auto cursor-pointer"
+      onClick={() => router.push("/pmf")}
+    >
       <Image width="189" height="23" src={logoUrl} alt="groundbreak app" />
     </div>
   </>)
