24e37df922b6cd16aca638a8d25a9873d75e1ffd
baymax <pony.kadeem@gmail.com>
2023-06-01 16:26:52 -0400

    chore: logo width, height

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -8,7 +8,7 @@ export const Logo = () => {
     <div className="w-[189px] h-[23px] flex items-center absolute left-0 right-0 bottom-4 sm:left-8 sm:bottom-8 p-1 justify-center ml-auto mr-auto sm:ml-0 z-[9999] pointer-events-auto cursor-pointer"
       onClick={() => router.push("/pmf")}
     >
-      <Image width="189" height="23" src={logoUrl} alt="groundbreak app" />
+      <Image fill src={logoUrl} alt="groundbreak app" />
     </div>
   </>)
 }
