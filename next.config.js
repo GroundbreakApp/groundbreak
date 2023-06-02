9c1402ddd5a218ffe9fe861061a307ffdfdc9f3c
zfr77 <zfr@zbalbous.com>
2023-06-02 21:45:22 +0100

    main redirect to webflow

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/index.html',
+        destination: 'https://groundbreak-landing-page.webflow.io/',
       },
       {
         source: '/pmf',
