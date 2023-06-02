5cf1d44100ec726db3718d0880d61fb414aa1e4b
baymax <pony.kadeem@gmail.com>
2023-06-02 10:06:27 -0400

    feat: ph1, ph2

---
+++ pages/ph1.tsx
@@ -0,0 +1,204 @@
+
+import React, { Suspense, useState, useEffect } from "react";
+import MyStory from '@/components/my-story'
+import LinkSVG from "@/assets/link.svg";
+import styled from 'styled-components';
+import axios from 'axios';
+import { TbSend } from "react-icons/tb"
+import { AiOutlineCheck, AiOutlineLoading3Quarters } from "react-icons/ai";
+
+import { isMobile } from 'react-device-detect';
+
+import linkImage from '../assets/Subtract.png';
+import UpLeft from '../assets/up-left.svg';
+import { useAppDispatch } from "@/stores/hook";
+import { setPause } from "@/components/my-story/slices/story.slice";
+import { useJoinWaitList } from "@/services/api";
+import { RegisterForm } from "@/components/RegisterForm";
+import { Logo } from "@/components/logo";
+
+const StyledForm = styled.form`
+  position: absolute;
+  bottom: 8px;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.5); // semi-transparent background
+  padding: 10px;
+`;
+
+const StyledInput = styled.input`
+  box-sizing: border-box;
+  flex-grow: 1;
+  background: transparent;
+  border: none;
+  font-family: 'Graphik';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 25px;
+  color: white;
+`;
+
+const StyledIcon = styled.div`
+  width: 25px;
+  height: 25px;
+  opacity: 0.5;
+  background: #33363F;
+  border-radius: 1px;
+`;
+
+const FeedbackForm = () => {
+  const [feedbackText, setFeedbackText] = useState(''); // Add state for feedback text
+  const handleIconClick = async () => {
+
+    console.log("Submit clicked")
+    try {
+      console.log("Making call to send feedback")
+
+      const response = await axios.post('https://groundbreak.onrender.com/metrics/feedback', {
+        text: feedbackText,
+      });
+
+      console.log(response)
+      setFeedbackText('');
+    } catch (error) {
+      console.error('Error sending feedback call:', error);
+    }
+  };
+  const dispatch = useAppDispatch();
+  return (
+    <div style={{
+      position: "absolute",
+      left: 0,
+      right: 0,
+      width: "100%",
+      height: "100%",
+      zIndex: "20000",
+    }}>
+      <StyledForm>
+        <StyledInput
+          placeholder="Send Feedback"
+          value={feedbackText} // Set the value of StyledInput to feedbackText state
+          onChange={(event: any) => setFeedbackText(event.target.value)} // Update feedbackText state on input change
+          onFocus={() => dispatch(setPause(true))}
+        />
+        {/* <StyledIcon onClick={handleIconClick as () => void} style={{ backgroundImage: `url(${linkImage})` }} /> */}
+        <button className="h-8 w-8 text-white fill-current"
+          onClick={handleIconClick}
+        >
+
+          <TbSend />
+        </button>
+      </StyledForm>
+    </div>
+  )
+}
+
+const stories2 = [
+  {
+    playbackId: "i4f025tuVa2024JMiniMEEdR013EVwhbJpJK800ehk4pVmc",
+    duration: 13 * 1000,
+    type: "mux-video",
+    isAutoplay: false,
+    overlayColor: "#45476d"
+  },
+
+  {
+    playbackId: "3P00vfla6A2ds7wBnZIsZEDenPent9RwobTDi3GYWEfI", // groundbreak is short form video
+    type: "mux-video",
+    duration: 15.8 * 1000,
+    isAutoplay: true,
+    overlayColor: "#f7d4e2",
+  },
+  {
+    playbackId: "Lrt72Y00TT2XyT6tqZ8OxbLDatFNt02eBLvbuAAju7LD00", // groundbreak is short form video
+    type: "mux-video",
+    duration: 7.9 * 1000,
+    isAutoplay: true,
+    overlayColor: "#d09269",
+    widgets: [
+      {
+        spawnTime: 0,
+        duration: 7000,
+        render: () => <RegisterForm />
+      }
+    ]
+  },
+  {
+    playbackId: "Mahil2YLAqtLr0101txaKF1fw5mJrWMl54SKIy302zW02uw", // groundbreak is short form video
+    type: "mux-video",
+    duration: 5.2 * 1000,
+    isAutoplay: true,
+    overlayColor: "#000000",
+  },
+];
+
+
+function App() {
+  var loadIgnore = false;
+
+  const [height, setHeight] = useState('100vh'); // default to vh
+
+  useEffect(() => {
+    if ('CSS' in window && CSS.supports('height', '100svh')) {
+      setHeight('100svh'); // switch to svh if supported
+    }
+
+    if (!loadIgnore) {
+      const searchParams = new URLSearchParams(location.search);
+      const parameterValue = searchParams.get('clicksrc');
+
+      var clicksrc = 'none'
+      var deviceType = 'desktop'
+      if (parameterValue) {
+        clicksrc = parameterValue
+      }
+      if (isMobile) {
+        deviceType = 'mobile'
+      }
+      console.log("Making post call")
+
+      const makeCall = async () => {
+        try {
+          var response = await axios.post('https://groundbreak.onrender.com/metrics/tracking', {
+            deviceType: deviceType,
+            clickSrc: clicksrc,
+          });
+          console.log(response)
+        } catch (error) {
+          console.error('Error making call:', error);
+        }
+      };
+      makeCall()
+
+    }
+    loadIgnore = true
+  }, []);
+
+  return (
+    <div className="App" style={{ display: 'grid', justifyContent: 'center', alignItems: 'center', height }}>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+      <div className="stories">
+        <Suspense>
+          <MyStory
+            height="100svh"
+            width="100svw"
+            keyboardNavigation
+            defaultInterval={8000}
+            stories={stories2}
+            onStoryEnd={(s: any, st: any) => { }}
+            onAllStoriesEnd={(s: any, st: any) => { }}
+            onStoryStart={(s: any, st: any) => { }}
+            storyContainerStyles={{ borderRadius: 8, overflow: "hidden" }}
+          />
+          <Logo />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ pages/ph2.tsx
@@ -0,0 +1,159 @@
+
+import React, { Suspense, useState, useEffect } from "react";
+import MyStory from '@/components/my-story'
+import UpLeft from "@/assets/up-left.svg";
+import styled from 'styled-components';
+import axios from 'axios';
+import { TbSend } from "react-icons/tb"
+
+import { isMobile } from 'react-device-detect';
+
+import { useAppDispatch } from "@/stores/hook";
+import { setPause } from "@/components/my-story/slices/story.slice";
+import { RegisterForm } from "@/components/RegisterForm";
+import { Logo } from "@/components/logo";
+
+
+const stories2 = [
+  {
+    playbackId: "i4f025tuVa2024JMiniMEEdR013EVwhbJpJK800ehk4pVmc",
+    duration: 13 * 1000,
+    type: "mux-video",
+    isAutoplay: false,
+    overlayColor: "#45476d"
+  },
+
+  {
+    playbackId: "txu6bFcw502HhhA2SkZc14pUujOwQi1lZzrNEIkULIkQ", // groundbreak is short form video
+    type: "mux-video",
+    duration: 4.3 * 1000,
+    isAutoplay: true,
+    overlayColor: "#222c67",
+  },
+  {
+    playbackId: "pV02Txnt7SFyO6OAhH3W5o6IWwd5Zpfc02r7meaKimPt00", // groundbreak is short form video
+    type: "mux-video",
+    duration: 3.8 * 1000,
+    isAutoplay: true,
+    overlayColor: "#664636",
+  },
+  {
+    playbackId: "3I9gOOM2LpVqjOv00ejZx3nYQ2q5xluD4HtSez02MDN00w", // groundbreak is short form video
+    type: "mux-video",
+    duration: 4.2 * 1000,
+    isAutoplay: true,
+    overlayColor: "#b8a0da",
+  },
+  {
+    playbackId: "b5NrVCRkAobl02NKKtf96HkXzIKzdnBuRlKopG8ZZRpI", // groundbreak is short form video
+    type: "mux-video",
+    duration: 3.7 * 1000,
+    isAutoplay: true,
+    overlayColor: "#ebc38e",
+    widgets: [
+      {
+        spawnTime: 0,
+        duration: 7000,
+        render: () => {
+          return <div className="flex justify-center items-start h-full w-full absolute left-0 top-8 z-[99999] ">
+            <a href="http://pmf.highalpha.com" target="_blank" className="mt-4 bg-[#FFBB86] text-[#172358] rounded-lg font-semibold py-2 flex items-center justify-center px-2">
+              View the Report
+              <span className="ml-2 w-4 h-4"> <UpLeft /> </span>
+            </a>
+          </div>
+        }
+      },
+    ]
+  },
+  {
+    playbackId: "4W00hZ4pMCakw02ndar56omDgA5sWguaflfc002ckR7qwU", // groundbreak is short form video
+    type: "mux-video",
+    duration: 7.9 * 1000,
+    isAutoplay: true,
+    overlayColor: "#ffffff",
+    widgets: [
+      {
+        spawnTime: 0,
+        duration: 7000,
+        render: () => <RegisterForm />
+      }
+    ]
+  },
+  {
+    playbackId: "Mahil2YLAqtLr0101txaKF1fw5mJrWMl54SKIy302zW02uw", // groundbreak is short form video
+    type: "mux-video",
+    duration: 5.2 * 1000,
+    isAutoplay: true,
+    overlayColor: "#000000",
+  },
+];
+
+
+function App() {
+  var loadIgnore = false;
+
+  const [height, setHeight] = useState('100vh'); // default to vh
+
+  useEffect(() => {
+    if ('CSS' in window && CSS.supports('height', '100svh')) {
+      setHeight('100svh'); // switch to svh if supported
+    }
+
+    //window.addEventListener('touchstart', function(e){ e.preventDefault(); });
+    window.addEventListener('touchmove', function (e) { e.preventDefault(); }, { passive: false });
+
+    if (!loadIgnore) {
+      const searchParams = new URLSearchParams(location.search);
+      const parameterValue = searchParams.get('clicksrc');
+
+      var clicksrc = 'none'
+      var deviceType = 'desktop'
+      if (parameterValue) {
+        clicksrc = parameterValue
+      }
+      if (isMobile) {
+        deviceType = 'mobile'
+      }
+      console.log("Making post call")
+
+      const makeCall = async () => {
+        try {
+          var response = await axios.post('https://groundbreak.onrender.com/metrics/tracking', {
+            deviceType: deviceType,
+            clickSrc: clicksrc,
+          });
+          console.log(response)
+        } catch (error) {
+          console.error('Error making call:', error);
+        }
+      };
+      makeCall()
+
+    }
+    loadIgnore = true
+  }, []);
+
+
+
+  return (
+    <div className="App" style={{ display: 'grid', justifyContent: 'center', alignItems: 'center', height, }}>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+      <div className="stories">
+        <MyStory
+          height="100svh"
+          width="100svw"
+          keyboardNavigation
+          defaultInterval={8000}
+          stories={stories2}
+          onStoryEnd={(s: any, st: any) => { }}
+          onAllStoriesEnd={(s: any, st: any) => { }}
+          onStoryStart={(s: any, st: any) => { }}
+          storyContainerStyles={{ borderRadius: 8, overflow: "hidden" }}
+        />
+        <Logo />
+      </div>
+    </div>
+  );
+}
+
+export default App;
