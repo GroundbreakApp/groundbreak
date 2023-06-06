bda224a3b781856a33e2704e20bc1288435f2ec5
baymax <pony.kadeem@gmail.com>
2023-06-06 09:45:16 -0400

    chore: linkedin inspector

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
+        <meta property="og:image:secure_url" content="https://groundbreak-qa.vercel.app/images/GB-Newsletter.gif" />
+        <meta property="og:image:type" content="image/gif" />
+        <meta property="og:image:width" content="270" />
+        <meta property="og:image:height" content="480" />
       </Head>
       <body
       >
