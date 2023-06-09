bf53bf9b1c6b7e937a73271aa9cf7fb93d79422d
baymax <pony.kadeem@gmail.com>
2023-06-09 19:40:14 -0400

    chore: mobile issue

--- components/new-story/components/ContainerMobile.tsx
+++ components/new-story/components/ContainerMobile.tsx
@@ -155,7 +155,7 @@ export default function ContainerMobile() {
       </div>
       {muted && <UnMute />}
       {/** Mux Video player */}
-      <div className="m-auto h-full flex rounded-3xl overflow-hidden w-full sm:w-[300px] sm:h-[532px] z-[9999]" style={{
+      <div className="absolute left-0 right-0 top-0 bottom-0 m-auto h-full flex rounded-3xl overflow-hidden w-full sm:w-[300px] sm:h-[532px] z-[9999]" style={{
         opacity: pause ? 0 : 1,
       }}>
         <MuxPlayer
