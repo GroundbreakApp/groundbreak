babde04fb4d0fc552b3d19e9ef54e9f7669219de
baymax <pony.kadeem@gmail.com>
2023-06-21 12:01:30 -0400

    Revert "fix: center logo position"
    This reverts commit fec941de0fd051ff80c896f2f3faf4c0e3bc990c.

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 export const Logo = () => {
   const router = useRouter();
   return (<>
-    <div className="w-[189px] h-[23px] flex items-center absolute left-0 right-0 bottom-6  p-1 justify-center ml-auto mr-auto  z-[9999] pointer-events-auto cursor-pointer"
+    <div className="w-[189px] h-[23px] flex items-center absolute left-0 right-0 bottom-6 sm:left-8 sm:bottom-8 p-1 justify-center ml-auto mr-auto sm:ml-0 z-[9999] pointer-events-auto cursor-pointer"
       onClick={() => router.push("/pmf")}
     >
       <Image fill src={logoUrl} alt="groundbreak app" />
